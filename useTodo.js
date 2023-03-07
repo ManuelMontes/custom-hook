572a3226c63425ee3aaa16c4bb5db73e29a9fb08
ManuelMontes <jose_m_montes96@hotmail.com>
2023-03-06 22:59:58 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/TodoReducer";
+
+const initialState = [];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+    
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+    
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos) || [])
+        // console.log(todos)
+    }, [todos])
+
+    const handleNewTodo = ( todo ) => {
+        // console.log({todo});´
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+
+    const handleToggleTodo = (id) => {
+        // console.log({id});        
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
+    // const todosCounter = todos.length;
+    // const pending = todos.filter(todo => !todo.done).length
+
+  return {
+    handleNewTodo,
+    todosCounter: todos.length,
+    pending: todos.filter(todo => !todo.done).length,
+    handleDeleteTodo,
+    handleToggleTodo,
+    todos,
+    // todosCounter,
+    // pending,
+  }
+}
